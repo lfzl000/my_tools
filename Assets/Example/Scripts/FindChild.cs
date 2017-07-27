337b9211b3f4d33115206166475fa0e32782cec7
zhaoliang <zhaoliang@bimsop.com>
2017-07-27 14:42:38 +0800

    添加案例场景1，2

---
+++ Assets/Example/Scripts/FindChild.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindChild : MonoBehaviour
+{
+    public string childName;
+
+    void Start()
+    {
+        var child = FindChildTool.FindChild<Renderer>(transform, childName);
+        if (child)
+        {
+            child.material.color = Color.red;
+            Debug.Log("找到物体" + childName);
+        }
+        else
+            Debug.Log("没有找到物体" + childName);
+    }
+}
---
+++ Assets/Example/Scripts/Folder.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Folder : MonoBehaviour
+{
+    public void OpenFolderExample()
+    {
+        Application.OpenURL("https://github.com/lfzl000/Folder.git");
+    }
+}
